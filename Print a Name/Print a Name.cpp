31dcd94340248ff6f9346e59102b428aacaa162a
Simplerdevs <tplg7839@gmail.com>
2024-06-06 06:25:44 -0700

    Add project files.

---
+++ Print a Name/Print a Name.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+						/* A program to ask the user to enter His/Her name and print it on screen */
+int main()
+
+{
+
+	string name;
+	cout << "Please enter your name ! " << endl;
+
+	cin >> name;
+	cout << "Entered name is : " << name << endl;
+
+	return 0;
+
+	// created by @ilyes_trabelsi
+}
